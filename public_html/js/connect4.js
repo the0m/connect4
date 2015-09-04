f1c9665b631ff7b47f13e1e2875a4d3bdcff04fe
the0m <mssiii@charter.net>
2015-09-04 10:35:46 -0500

    Update connect4.js

--- public_html/js/connect4.js
+++ public_html/js/connect4.js
@@ -297,7 +297,7 @@ function fillArray() {
 //[columns][rows]
 function winCondition() {
     //horizontal
-    for (var i = 1; i < 5; i++) {
+    for (var i = 1; i < 4; i++) {
         for (var j = 1; j < 7; j++) {
             if (pos_array[i][j] !== undefined && pos_array[i][j] === pos_array[i + 1][j] && pos_array[i][j] === pos_array[i + 2][j] && pos_array[i][j] === pos_array[i + 3][j]) {
                 win(i, j, "h");
